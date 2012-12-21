de65b02ff11df8886f8f63dfdbb64edd3229f4b5
Jeffrey Drake <drakej@uwindsor.ca>
2012-12-21 18:13:50 -0500

    Adding matrix subtract, adding and subtraction scalar

--- main.cpp
+++ main.cpp
@@ -11,30 +11,28 @@
 
 #include "Matrix.h"
 
-using namespace std;
-
 /*
  * 
  */
 int main(int argc, char** argv) {
-    cout << "Testing" << endl;
-    
-    drakej::Matrix<float> a(3,3, {1,2,3,4,5,6,7,8,9});
-    drakej::Matrix<float> b(3,3, {1,1,1,1,1,1,1,1,1});
-    
-//    std::vector<float> c { 1, 2, 3, 4 };
-    
-//    auto x = drakej::leading_digits(c);
-    
-//    cout << "Digits: " << ceil(log10(1001)) << endl;
-//    cout << "Digits: " << x << endl;
-    
-    cout << " a = " << endl << a << endl;
-    cout << " b = " << endl << b << endl;
-    
-    auto c = a + b;
-    cout << " a + b = " << endl << a + b << endl;
-    
+    std::cout << "Testing" << std::endl;
+
+    drakej::Matrix<float> a(3, 3,{1, 2, 3, 4, 5, 6, 7, 8, 9});
+    drakej::Matrix<float> b(3, 3,{1, 1, 1, 1, 1, 1, 1, 1, 1});
+
+    //    std::vector<float> c { 1, 2, 3, 4 };
+
+    //    auto x = drakej::leading_digits(c);
+
+    //    cout << "Digits: " << ceil(log10(1001)) << endl;
+    //    cout << "Digits: " << x << endl;
+
+    std::cout << " a = " << std::endl << a << std::endl;
+    std::cout << " b = " << std::endl << b << std::endl;
+
+    auto c = b - 1;
+//    std::cout << " a + b = " << std::endl << c << std::endl;
+
     return 0;
 }
 
